46568a7fbe9ad027b2edff6f04d11d475601ba03
SoufianKm <soufian.kamma@gmail.com>
2024-04-05 04:13:00 -0700

    Full deployment

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Fabric script (based on the file 2-do_deploy_web_static.py)
+that creates and distributes an archive to your web
+servers, using the function deploy
+"""
+
+from fabric.api import put, run, env, local
+from datetime import datetime
+from os.path import exists, isdir
+env.hosts = ['54.90.57.18', '35.153.51.85']
+
+
+def do_pack():
+    """
+    compressed archive file of web_static folder
+    """
+    ref = 'web_static_' + datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = ref + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -czvf versions/{} web_static'.format(file_name))
+    if create.succeeded:
+        return 'versions/{}'.format(file_name)
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        # essential file names
+        file_name = archive_path.split("/")[-1]
+        extract_folder = file_name.split(".")[0]
+        full_path = "/data/web_static/releases/"
+        link = "/data/web_static/current"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(full_path, extract_folder))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, full_path,
+            extract_folder))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(full_path, extract_path))
+        run('rm -rf {}{}/web_static'.format(full_path, extract_path))
+        run('rm -rf {}'.format(link))
+        run('ln -s {}{}/ {}'.format(full_path, extract_path, link))
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """creates and distributes an archive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
