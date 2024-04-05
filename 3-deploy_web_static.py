d66f53866b36b044f7245112037f0d5dd09b2575
SoufianKm <soufian.kamma@gmail.com>
2024-04-05 05:19:46 -0700

    Update

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -18,7 +18,7 @@ def do_pack():
     ref = 'web_static_' + datetime.now().strftime("%Y%m%d%H%M%S")
     file_name = ref + '.' + 'tgz'
     local('mkdir -p versions')
-    create = local('tar -czvf versions/{} web_static'.format(file_name))
+    create = local('tar -cvzf versions/{} web_static'.format(file_name))
     if create.succeeded:
         return 'versions/{}'.format(file_name)
     else:
